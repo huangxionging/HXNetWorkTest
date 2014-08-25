194fbfa4d689075072176fe9883a50e6d623e6d0
huangxiong <huangxiong@huangxiong.local>
2014-08-25 10:28:32 +0800

    Initial Commit

---
+++ HXNetWorkTest/HXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HXAppDelegate.h
+//  HXNetWorkTest
+//
+//  Created by huangxiong on 14-8-25.
+//  Copyright (c) 2014年 New-Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
